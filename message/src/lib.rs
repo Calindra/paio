f0f1ccb302ea4991642e017548b9932e82fcabb6
sandhilt <6170125+sandhilt@users.noreply.github.com>
2024-09-29 17:01:10 -0300

    revert: revert test of message

--- message/src/lib.rs
+++ message/src/lib.rs
@@ -360,76 +360,76 @@ pub struct SubmitPointTransaction {
     pub signature: String
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use alloy_core::sol_types::SolStruct;
-//     use alloy_signer::SignerSync;
-//     use alloy_signer_wallet::LocalWallet;
-//     use std::str::FromStr;
+#[cfg(test)]
+mod tests {
+    use alloy_core::sol_types::SolStruct;
+    use alloy_signer::SignerSync;
+    use alloy_signer_local::PrivateKeySigner as LocalWallet;
+    use std::str::FromStr;
 
-//     use super::*;
+    use super::*;
 
-//     fn produce_tx() -> (String, Address) {
-//         let json = r#"
-//         {
-//             "app":"0x0000000000000000000000000000000000000000",
-//             "nonce":0,
-//             "max_gas_price":0,
-//             "data":"0x48656c6c6f2c20576f726c6421"
-//         }
-//         "#;
+    fn produce_tx() -> (String, Address) {
+        let json = r#"
+        {
+            "app":"0x0000000000000000000000000000000000000000",
+            "nonce":0,
+            "max_gas_price":0,
+            "data":"0x48656c6c6f2c20576f726c6421"
+        }
+        "#;
 
-//         let v: SigningMessage = serde_json::from_str(json).unwrap();
-//         let signer = LocalWallet::from_str(
-//             "8114fae7aa0a92c7e3a6015413a54539b4ba9f28254a70f67a3969d73c33509b",
-//         )
-//         .unwrap();
-//         assert_eq!(
-//             alloy_core::hex::encode(signer.to_field_bytes()),
-//             "8114fae7aa0a92c7e3a6015413a54539b4ba9f28254a70f67a3969d73c33509b"
-//         );
-//         assert_eq!(
-//             "0x7306897365c277A6951FDA9519fD0CCc16341E4A",
-//             signer.address().to_string()
-//         );
+        let v: SigningMessage = serde_json::from_str(json).unwrap();
+        let signer = LocalWallet::from_str(
+            "8114fae7aa0a92c7e3a6015413a54539b4ba9f28254a70f67a3969d73c33509b",
+        )
+        .unwrap();
+        assert_eq!(
+            alloy_core::hex::encode(signer.to_field_bytes()),
+            "8114fae7aa0a92c7e3a6015413a54539b4ba9f28254a70f67a3969d73c33509b"
+        );
+        assert_eq!(
+            "0x7306897365c277A6951FDA9519fD0CCc16341E4A",
+            signer.address().to_string()
+        );
 
-//         let signature = signer.sign_typed_data_sync(&v, &DOMAIN).unwrap();
-//         assert_eq!(
-//             r#"{"r":"0xfa6f7fd6825c953b355c8970fd2c9322162987bfb6898aa78f74f2be6bf8b10c","s":"0x9a2018a7e31b623a91802147e6f8d5c658e17191e69f6663052efda71db72e2","yParity":"0x1"}"#,
-//             serde_json::to_string(&signature).unwrap()
-//         );
-//         let signed_tx = SignedTransaction {
-//             message: v,
-//             signature,
-//         };
+        let signature = signer.sign_typed_data_sync(&v, &DOMAIN).unwrap();
+        assert_eq!(
+            r#"{"r":"0xfa6f7fd6825c953b355c8970fd2c9322162987bfb6898aa78f74f2be6bf8b10c","s":"0x9a2018a7e31b623a91802147e6f8d5c658e17191e69f6663052efda71db72e2","yParity":"0x1"}"#,
+            serde_json::to_string(&signature).unwrap()
+        );
+        let signed_tx = SignedTransaction {
+            message: v,
+            signature,
+        };
 
-//         let ret = serde_json::to_string(&signed_tx).unwrap();
+        let ret = serde_json::to_string(&signed_tx).unwrap();
 
-//         assert_eq!(
-//             r#"{"message":{"app":"0x0000000000000000000000000000000000000000","nonce":0,"max_gas_price":0,"data":"0x48656c6c6f2c20576f726c6421"},"signature":{"r":"0xfa6f7fd6825c953b355c8970fd2c9322162987bfb6898aa78f74f2be6bf8b10c","s":"0x9a2018a7e31b623a91802147e6f8d5c658e17191e69f6663052efda71db72e2","yParity":"0x1"}}"#,
-//             ret
-//         );
+        assert_eq!(
+            r#"{"message":{"app":"0x0000000000000000000000000000000000000000","nonce":0,"max_gas_price":0,"data":"0x48656c6c6f2c20576f726c6421"},"signature":{"r":"0xfa6f7fd6825c953b355c8970fd2c9322162987bfb6898aa78f74f2be6bf8b10c","s":"0x9a2018a7e31b623a91802147e6f8d5c658e17191e69f6663052efda71db72e2","yParity":"0x1"}}"#,
+            ret
+        );
 
-//         (ret, signer.address())
-//     }
+        (ret, signer.address())
+    }
 
-//     #[test]
-//     fn test() {
-//         let (tx_json, signer) = produce_tx(); // metamask
-//         println!("JSON: {tx_json}");
+    #[test]
+    fn test() {
+        let (tx_json, signer) = produce_tx(); // metamask
+        println!("JSON: {tx_json}");
 
-//         let tx: SignedTransaction = serde_json::from_str(&tx_json).unwrap();
-//         let signing_hash = tx.message.eip712_signing_hash(&DOMAIN);
-//         let recovered = tx
-//             .signature
-//             .recover_address_from_prehash(&signing_hash)
-//             .unwrap();
+        let tx: SignedTransaction = serde_json::from_str(&tx_json).unwrap();
+        let signing_hash = tx.message.eip712_signing_hash(&DOMAIN);
+        let recovered = tx
+            .signature
+            .recover_address_from_prehash(&signing_hash)
+            .unwrap();
 
-//         assert_eq!(signer, recovered);
+        assert_eq!(signer, recovered);
 
-//         assert_eq!(
-//             r#"{"name":"CartesiPaio","version":"0.0.1","chainId":"0x539","verifyingContract":"0x0000000000000000000000000000000000000000"}"#,
-//             serde_json::to_string(&DOMAIN).unwrap()
-//         );
-//     }
-// }
+        assert_eq!(
+            r#"{"name":"CartesiPaio","version":"0.0.1","chainId":"0x539","verifyingContract":"0x0000000000000000000000000000000000000000"}"#,
+            serde_json::to_string(&DOMAIN).unwrap()
+        );
+    }
+}
